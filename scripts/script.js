bf414d66233590bf92609f642265bd472ea80752
amucinom <amucinom@gmail.com>
2016-04-18 13:32:30 -0700

    Fixed info sections on main page and fixed colors

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function($) {
 
-    $(window).scroll(function() {
-    	var top = $(document).scrollTop();
-        if (top > 0) {
-            $('.navbar').addClass('navbar-shadow');
-        } else {
-            $('.navbar').removeClass('navbar-shadow');
-        }
-    });
+    // $(window).scroll(function() {
+    // 	var top = $(document).scrollTop();
+    //     if (top > 0) {
+    //         $('.navbar').addClass('navbar-shadow');
+    //     } else {
+    //         $('.navbar').removeClass('navbar-shadow');
+    //     }
+    // });
 
 });
