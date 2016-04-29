c65943410fd46aba3d2c92505fdb86f4c6c5e8e0
amucinom <amucinom@gmail.com>
2016-04-28 20:08:33 -0700

    Set top logo to disappear when scrolling down when on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,28 @@
 $(document).ready(function($) {
 
     $(window).scroll(function() {
-        $(window).resize(function() {
+        // $(window).resize(function() {
             if ($(window).width() < 481) {
                 var top = $(document).scrollTop();
                 if (top > 0) {
-                    $('.dd').css('display', 'none');
+                    $('.navbar-logo').css('display', 'none');
+                    $('.navbar').css({
+                    	'padding-top': '0',
+                    	'transition': 'all 0.25s'
+                    });
+                    $('.navbar-nav').css('border', 'none');
                 }
-                // else {
-                //     $('.navbar').removeClass('navbar-shadow');
-                // }
+                else {
+                    $('.navbar-logo').css('display', 'block');
+                    $('.navbar').css({
+                    	'padding-top': '0.5rem',
+                    	'transition': 'all 0.25s'
+                    });
+                    $('.navbar-nav').css('border-top', '0.075rem solid rgba(255,255,255,0.25)');
+                }
             }
 
-        });
+        // });
     });
 
 });
